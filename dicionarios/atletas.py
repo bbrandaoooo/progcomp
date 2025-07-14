fedb1b402c91c72e40e46b2be15d8dd8321ff556
bbrandaoooo <brandaopedrob@gmail.com>
2025-07-14 20:48:10 -0300

    adicionado o acesso às posições e o id da posição de cada jogador

--- dicionarios/atletas.py
+++ dicionarios/atletas.py
@@ -8,8 +8,22 @@ try:
         if atleta["clube_id"] == 263: # Botafogo/RJ
             print (f"{atleta['apelido']} -> {atleta['nome']}")
     '''
+    
+    # Mapeamento de posições:
+    posicoes = {
+        1: "Goleiro",
+        2: "Lateral",
+        3: "Zagueiro",
+        4: "Meia",
+        5: "Atacante",
+        6: "Técnico"
+    }
+    
     botafogo = filter (lambda x: x['clube_id'] == 263, atletas)
     for atleta in botafogo:
-        print (f"{atleta['apelido']} -> {atleta['nome']}")
+        posicao_id = atleta['posicao_id']
+        posicao_nome = posicoes.get(posicao_id, 'Desconhecida')
+        print('\n')
+        print (f"{atleta['apelido']} -> {atleta['nome']} | Posição: {posicao_nome} ({posicao_id})")
 except json.decoder.JSONDecodeError as e:
     print ("Erro na conversão de JSON para dicionario")
