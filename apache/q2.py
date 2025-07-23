8718d31bfd5bbb665c98411c53a707305ca809b4
bbrandaoooo <brandaopedrob@gmail.com>
2025-07-23 17:51:33 -0300

    ok

--- apache/q2.py
+++ apache/q2.py
@@ -13,7 +13,8 @@ try:
         else:
             logsMinuto[tempo] = 1
     fdLogs.close()
-    print(max (logsMinuto.values()))
+    maisOcorre = (max (logsMinuto.items(), key=lambda item: item[1]))
+    print(f"A data com mais acessos é {maisOcorre[0]} com {maisOcorre[1]} acessos.")
 except FileNotFoundError as e:
     print(f"O arquivo {nomeArquivo} não está acessível")
     exit()
